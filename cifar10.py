c720477747249e117634e10da016fed3bda439e8
fukuta0614 <fukuta6140@gmail.com>
2016-09-24 14:14:06 +0900

    asdf

--- cifar10.py
+++ cifar10.py
@@ -71,7 +71,7 @@ def run(model_path, batch_size, num_epoch, data_augmentation):
                   metrics=['accuracy'],
                   )
 
-    early_stopping = EarlyStopping(monitor='val_loss', patience=2)
+    early_stopping = EarlyStopping(monitor='val_loss', patience=3)
 
     if not data_augmentation:
         print('Not using data augmentation.')
@@ -86,14 +86,14 @@ def run(model_path, batch_size, num_epoch, data_augmentation):
 
         # this will do preprocessing and realtime data augmentation
         datagen = ImageDataGenerator(
-            featurewise_center=True,  # set input mean to 0 over the dataset
+            featurewise_center=False,  # set input mean to 0 over the dataset
             samplewise_center=False,  # set each sample mean to 0
-            featurewise_std_normalization=True,  # divide inputs by std of the dataset
+            featurewise_std_normalization=False,  # divide inputs by std of the dataset
             samplewise_std_normalization=False,  # divide each input by its std
             zca_whitening=False,  # apply ZCA whitening
-            rotation_range=10,  # randomly rotate images in the range (degrees, 0 to 180)
-            width_shift_range=0.2,  # randomly shift images horizontally (fraction of total width)
-            height_shift_range=0.2,  # randomly shift images vertically (fraction of total height)
+            rotation_range=5,  # randomly rotate images in the range (degrees, 0 to 180)
+            width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
+            height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
             horizontal_flip=True,  # randomly flip images
             vertical_flip=False)  # randomly flip images
 

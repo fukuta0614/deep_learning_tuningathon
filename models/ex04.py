e13d939b0793a8952ea0913b938b8f987b2225c7
fukuta0614 <fukuta6140@gmail.com>
2016-09-24 14:21:29 +0900

    asdf

--- models/ex04.py
+++ models/ex04.py
@@ -5,7 +5,7 @@ from __future__ import print_function
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Convolution2D, MaxPooling2D
-
+from keras.layers.normalization import BatchNormalization
 
 def create_network(input_shape, output_shape):
     """
@@ -18,15 +18,19 @@ def create_network(input_shape, output_shape):
     model = Sequential()
 
     model.add(Convolution2D(32, 3, 3, border_mode='same', input_shape=input_shape))
+    model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(Convolution2D(32, 3, 3, border_mode='same'))
+    model.add(BatchNormalization())
     model.add(Activation('relu'))
 
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
     model.add(Convolution2D(64, 3, 3, border_mode='same'))
+    model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(Convolution2D(64, 3, 3, border_mode='same'))
+    model.add(BatchNormalization())
     model.add(Activation('relu'))
 
     model.add(MaxPooling2D(pool_size=(2, 2)))

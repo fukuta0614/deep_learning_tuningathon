b9e270b8809457d0c474a67e3f3979b783e25739
fukuta0614 <fukuta6140@gmail.com>
2016-09-24 13:44:24 +0900

    asdf

--- models/ex04.py
+++ models/ex04.py
@@ -19,14 +19,14 @@ def create_network(input_shape, output_shape):
 
     model.add(Convolution2D(32, 3, 3, border_mode='same', input_shape=input_shape))
     model.add(Activation('relu'))
-    model.add(Convolution2D(32, 3, 3, border_mode='same')
+    model.add(Convolution2D(32, 3, 3, border_mode='same'))
     model.add(Activation('relu'))
 
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
-    model.add(Convolution2D(64, 3, 3, border_mode='same')
+    model.add(Convolution2D(64, 3, 3, border_mode='same'))
     model.add(Activation('relu'))
-    model.add(Convolution2D(64, 3, 3, border_mode='same')
+    model.add(Convolution2D(64, 3, 3, border_mode='same'))
     model.add(Activation('relu'))
 
     model.add(MaxPooling2D(pool_size=(2, 2)))
